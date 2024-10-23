4f160991d34c7aef1fccf1917fa913cbaff22037
Ayesha2917 <ayeshaahmed2912@gmail.com>
2024-10-23 20:23:47 +0500

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sum;
+
+/**
+ *
+ * @author light tech
+ */
+public class Sum {
+    public static void main(String[]args){
+        int n=10;
+        int sum=(n*(n+1))/2;
+        System.out.println("The sum of first "+n+" natural number is: "+sum);
+    }
+}
